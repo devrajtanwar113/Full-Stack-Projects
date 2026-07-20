ba61c0202ec697131b8e819ee272f9be96860a98
Devraj Singh <devrajtanwar113@gmail.com>
2026-07-20 10:43:34 +0530

    Added React Redux Social Media Post Composer

---
+++ src/app/store.js
@@ -0,0 +1,6 @@
+import {configureStore} from '@reduxjs/toolkit';
+import postsReducer from '../features/posts/postsSlice';
+import platformReducer from '../features/platform/platformSlice';
+export const store=configureStore({
+  reducer:{posts:postsReducer,platform:platformReducer,},
+});
---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,42 @@
+import {createSlice,createEntityAdapter,nanoid} from '@reduxjs/toolkit';
+
+const postsAdapter=createEntityAdapter({
+  sortComparer:(a,b)=>b.date.localeCompare(a.date),});
+const initialState=postsAdapter.getInitialState({
+  status:'idle',
+  error:null,});
+const postsSlice=createSlice({
+  name:'posts',
+  initialState,
+  reducers:{
+    postAdded:{
+      reducer:postsAdapter.addOne,
+      prepare:(content,platform)=>{
+        return{
+          payload:{
+            id:nanoid(),
+            content,
+            platform,
+            date:new Date().toISOString(),
+          },
+        };
+      },
+    },
+    postsReceived:postsAdapter.setAll,
+    postUpdated:postsAdapter.updateOne,
+    postDeleted:postsAdapter.removeOne,
+    postsCleared:postsAdapter.removeAll,
+  },});
+export const{
+  postAdded,
+  postsReceived,
+  postUpdated,
+  postDeleted,
+  postsCleared,
+}=postsSlice.actions;
+export const{
+  selectAll:selectAllPosts,
+  selectById:selectPostById,
+  selectIds:selectPostIds,
+}=postsAdapter.getSelectors((state)=>state.posts);
+export default postsSlice.reducer;
